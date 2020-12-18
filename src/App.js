9a537ae2bf5045b43c106751606c841ddc1cf459
NatanLee <afkolya@mail.ru>
2020-12-18 23:49:31 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TodoList from './Todo/TodoList';
 import Context from './context';
-import AddTodo from './Todo/addTodo';
+//import AddTodo from './Todo/AddTodo';
+import Loader from './Loader';
+import Modal from './Modal/Modal';
+
+const AddTodo = React.lazy(
+  () =>
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve(import('./Todo/AddTodo'))
+      }, 3000)
+    })
+)   
 
 //работа со state
 function App() {
   const [todos, setTodos] = React.useState([
-    {id: 1, completed: false, title: 'Купить хлеб'},
-    {id: 2, completed: true, title: 'Купить масло'},
-    {id: 3, completed: false, title: 'Купить молоко'}
+    //{id: 1, completed: false, title: 'Купить хлеб'},
+    //{id: 2, completed: true, title: 'Купить масло'},
+    //{id: 3, completed: false, title: 'Купить молоко'}
   ]);
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000)        
+      })
+  }, []);
   
   //let todos = 
 
@@ -28,13 +51,30 @@ function App() {
     setTodos(todos.filter(todo => todo.id !== id));
   }
 
+  function addTodo(title){
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      complete: false
+    }]))   
+  } 
+
 
   return (
     <Context.Provider value = {{removeTodo: removeTodo}}>
       <div className = "wrapper">
         <h1>React tutorial</h1>
-        <AddTodo />
-        {todos.length ? <TodoList todos = {todos} toggle = {toggleTodo}/> : <p>No todos</p>}
+        <Modal />
+        <React.Suspense fallback = {<p>Loading.....</p>}>
+          <AddTodo onCreate = {addTodo} />
+        </React.Suspense>        
+
+        {loading && <Loader />}
+        {todos.length ? (
+          <TodoList todos = {todos} toggle = {toggleTodo}/>
+        ) : (
+          loading ? null : <p>No todos</p>
+        )}
            
       </div>
     </Context.Provider>  
